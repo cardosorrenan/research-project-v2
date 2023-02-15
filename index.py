7318b7532ef8bddbd072283064f9807d8176a2dc
Renan Cardoso <renan.cardoso@advicehealth.com.br>
2023-02-15 01:46:37 -0300

    RRI to sampling frequency, get positive samples by aux_note

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 from operator import attrgetter
@@ -11,7 +11,7 @@ import neurokit2 as nk
 import numpy as np
 import wfdb
 
-# In[2]:
+# In[14]:
 
 
 def get_records_id(path: str) -> List[str]:
@@ -28,83 +28,106 @@ def get_records_id(path: str) -> List[str]:
     return list(map(lambda line: line.strip(), lines))
 
 
-def detect_r_peaks(signal_lead: np.ndarray, fs: int) -> np.ndarray:
-    """method to get the R-peak indices of a signal
+def get_intervals_afib(sample: List[int], aux_note: List[str]) -> List[List[int]]:
+    """
+    Get the intervals of atrial fibrillation (AFIB) from a list of sample values and corresponding annotations.
 
     Args:
-        signal_lead (np.ndarray): a signal recording coming from an ecg lead
-        fs (int): sampling rate
+    - sample (List[int]): A list of ECG sample values.
+    - aux_note (List[str]): A list of annotation labels for each sample.
 
     Returns:
-         np.ndarray: a numpy array containing found indices of R peaks
+    - afib_intervals (List[List[int]]): A list of start and end indices for each interval of AFIB.
     """
-    _, rpeaks = nk.ecg_peaks(signal_lead, sampling_rate=fs)
-    return rpeaks["ECG_R_Peaks"]
+    afib_intervals = []
+    for i, label in enumerate(aux_note):
+        if label == "(AFIB":
+            afib_start = sample[i]
+            if i + 1 < len(sample):
+                afib_end = sample[i + 1] - 1
+                afib_intervals.append([afib_start, afib_end])
+    return afib_intervals
 
 
-def get_boundaries_array(ecg_r_peaks: np.ndarray, signal_lead_size: int) -> np.ndarray:
-    """method to get boundaries array from ecg R-peaks and signal lead size
+def get_values_within_intervals(
+    intervals: List[List[int]], signal: List[float]
+) -> List[float]:
+    """
+    Extract values from a signal within the specified intervals.
+
+    Args:
+    - intervals (List[List[int]]): A list of start and end indices for each interval of interest.
+    - signal (List[float]): A list of values representing a signal.
+
+    Returns:
+    - values_within_intervals (List[float]): A list of values within the specified intervals.
+    """
+    values_within_intervals = []
+    for interval in intervals:
+        start_index, end_index = interval
+        if start_index < 0 or end_index >= len(signal) or start_index > end_index:
+            # Skip invalid intervals
+            continue
+        values_within_intervals.extend(signal[start_index : end_index + 1])
+    return values_within_intervals
+
+
+def extract_rri_signal(ecg_r_peaks: np.ndarray, signal_lead_size: int) -> List[float]:
+    """method to get IRR intervals from signal lead and founded R-peaks indices
 
     Args:
         ecg_r_peaks (np.ndarray): numpy array containing found R-peaks indices
-        signal_lead_size (int): size of the signal lead
+        signal_lead_size (int): a size of signal recording coming from an ECG lead
 
     Returns:
-        np.ndarray: numpy array containing found boundaries indices
+        List[float]: a list of floats, is the RRIs extracted from signal
     """
-    boundaries_array = np.zeros(ecg_r_peaks.size + 2, dtype=int)
-    boundaries_array[1:-1] = ecg_r_peaks
-    boundaries_array[-1] = signal_lead_size
-    return boundaries_array
+    delimiters = np.zeros(ecg_r_peaks.size + 2, dtype=int)
+    delimiters[1:-1] = ecg_r_peaks
+    delimiters[-1] = signal_lead_size
+    rri_signal = []
+    for i in range(delimiters.size - 1):
+        rri_beat = int(delimiters[i + 1] - delimiters[i])
+        rri_signal.append(rri_beat)
+    return rri_signal
 
 
-def get_irr_intervals(signal_lead, boundaries_array):
-    """method to get IRR intervals from signal lead and boundaries array
-
-    Args:
-        signal_lead (np.ndarray): a signal recording coming from an ECG lead
-        boundaries_array (np.ndarray): a numpy array containing found boundaries indices
-
-    Returns:
-        List[List[float]]: a list of list of floats, each sublist is an IRR interval
-    """
-    n_boundaries = boundaries_array.size
-    irr_intervals = []
-    for i in range(n_boundaries - 1):
-        irr_interval = signal_lead[boundaries_array[i] : boundaries_array[i + 1]]
-        irr_intervals.append(irr_interval.tolist())
-    return irr_intervals
-
-
-# In[3]:
-
-
-PATH_AFDB = "mit-bih-atrial-fibrillation-database-1.0.0/files"
-records_id_list = get_records_id(PATH_AFDB)
-
-records_id_list.remove("00735")
-records_id_list.remove("03665")
-
-for record_id in records_id_list:
-    path = f"{PATH_AFDB}/{record_id}"
-    record = wfdb.rdrecord(path)
-    sample, aux_note, fs = attrgetter("sample", "aux_note", "fs")(
-        wfdb.rdann(path, "atr")
-    )
-
-    leads = [0, 1]
-    for lead in leads:
-        signal_lead = record.p_signal[:, lead]
-        signal_lead_size = signal_lead.size
-        ecg_r_peaks = detect_r_peaks(signal_lead, fs)
-        boundaries_array = get_boundaries_array(ecg_r_peaks, signal_lead_size)
-        irr_intervals = get_irr_intervals(signal_lead, boundaries_array)
-
-        print(len(irr_intervals))
+def resample_ms(rri_signal, freq) -> List[float]:
+    MILISECONDS = 1000
+    return [(MILISECONDS / freq) * rri for rri in rri_signal]
 
 
 # In[5]:
 
 
-sample
+AFDB_PATH = "mit-bih-atrial-fibrillation-database-1.0.0/files"
 
+# Get list of record IDs
+record_ids = get_records_id(AFDB_PATH)
+
+# Remove problematic records
+record_ids.remove("00735")
+record_ids.remove("03665")
+
+# Process each record
+for record_id in record_ids:
+    record_path = f"{AFDB_PATH}/{record_id}"
+
+    # Load ECG signal and annotation data
+    ecg_signal, ecg_metadata = wfdb.rdsamp(record_path)
+
+    # Extract AFIB intervals from annotation data
+    sample, aux_note = attrgetter("sample", "aux_note")(wfdb.rdann(record_path, "atr"))
+    afib_intervals = get_intervals_afib(sample, aux_note)
+
+    # Process each lead in the ECG signal
+    for lead_idx, lead_signal in enumerate(ecg_signal.T):
+        # Segment signal into AFIB intervals and detect R-peaks
+        afib_signal = get_values_within_intervals(afib_intervals, lead_signal)
+        _, rpeaks = nk.ecg_peaks(afib_signal, sampling_rate=ecg_metadata["fs"])
+        ecg_rpeaks = rpeaks["ECG_R_Peaks"]
+
+        # Compute RRI signal and resample to 1 ms resolution
+        signal_len = len(lead_signal)
+        rri_signal = extract_rri_signal(ecg_rpeaks, signal_len)
+        rri_signal_ms = resample_ms(rri_signal, ecg_metadata["fs"])
